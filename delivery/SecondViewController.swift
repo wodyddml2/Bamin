b702ec4e7e56c0287e30886b4b8fc8040fb44f63
wodyddml2 <wodyddml2@naver.com>
2022-07-12 22:15:51 +0900

    새로운 스토리보드 추가

--- delivery/SecondViewController.swift
+++ delivery/SecondViewController.swift
@@ -7,39 +7,70 @@
 
 import UIKit
 
+
+
 class SecondViewController: UIViewController {
 
+    var timer = Timer()
+    
+    @IBOutlet weak var opacityView: UIView!
+    @IBOutlet weak var kakaoImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
+        
+        kakaoImage.layer.masksToBounds = true
+        kakaoImage.layer.cornerRadius = 30
+        
+        opacityView.layer.opacity = 0.2
 
-        // Do any additional setup after loading the view.
+        
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(true)
-        print("viewWillAppear")
+    
+    @IBAction func kakaoImageClicked(_ sender: UIButton) {
+        timer.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(counter), userInfo: nil, repeats: false)
+        
     }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(true)
-        print("viewDidAppear")
+    
+    @objc func counter() {
+        kakaoImage.layer.opacity = 0.8
+        
     }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
-        print("viewWillDisappear")
+    
+    @IBAction func call(_ sender: UIButton) {
+        showBox()
     }
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(true)
-        print("viewDidDisappear")
+    
+    func showBox() {
+        let alert = UIAlertController(title: "JY", message: "JY님에게 전화를 연결하시겠습니까?", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "전화걸기", style: .default, handler: nil)
+        let cancle = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(ok)
+        alert.addAction(cancle)
+        
+        present(alert, animated: true, completion: nil)
     }
+    
+//
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(true)
+//        print("viewWillAppear")
+//    }
+//    override func viewDidAppear(_ animated: Bool) {
+//        super.viewDidAppear(true)
+//        print("viewDidAppear")
+//    }
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(true)
+//        print("viewWillDisappear")
+//    }
+//    override func viewDidDisappear(_ animated: Bool) {
+//        super.viewDidDisappear(true)
+//        print("viewDidDisappear")
+//    }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+   
 
 }
