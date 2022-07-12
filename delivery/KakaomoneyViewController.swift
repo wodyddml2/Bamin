975af5f700c975443f02dd700cacdb39c43d27b3
wodyddml2 <wodyddml2@naver.com>
2022-07-12 23:07:46 +0900

    보충

---
+++ delivery/KakaomoneyViewController.swift
@@ -0,0 +1,22 @@
+//
+//  KakaomoneyViewController.swift
+//  delivery
+//
+//  Created by J on 2022/07/12.
+//
+
+import UIKit
+
+class KakaomoneyViewController: UIViewController {
+    @IBOutlet weak var kakaoImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
